142e90515e7950e52684a1363688c7b20cac0799
Kyrylo_Kud <kudkiril@gmail.com>
2025-09-12 17:42:50 +0200

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import aboutAutor from './assets/internet.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <div className=" text-light min-vh-100">
